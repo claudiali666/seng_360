5db0d92d2fab2b38e51b1f011b5fe251e450556d
cmaricle <courtneymaricle@gmail.com>
2017-11-07 15:07:12 -0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,55 +1,151 @@
-import java.io.*;
-import java.net.InetAddress;
-import java.net.Socket;
 
-public class Client
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class Server
 {
-    String host;
+    static final int CONFIDENTIALITY = 4;
+    static final int INTEGRITY = 2;
+    static final int AUTHENTICATION = 1;
+
+    static boolean confidentiality;
+    static boolean authenticaiton; 
+    static boolean integrity; 
+
+    static int optionsSelected;
+
     int port;
+    ServerSocket serverSocket;
 
     private static Socket socket;
 
-    public Client() throws IOException {
-        host = "localhost";
+    public Server() throws java.io.IOException{
         port = 8080;
-        InetAddress address = InetAddress.getByName(host);
-        socket = new Socket(address, port);
+        serverSocket = new ServerSocket(port);
+        System.out.println("Server Started and listening to the port 8080");
     }
 
-    public void sendMessage(String message) throws IOException {
-        //Send the message to the server
-        OutputStream os = socket.getOutputStream();
-        OutputStreamWriter osw = new OutputStreamWriter(os);
-        BufferedWriter bw = new BufferedWriter(osw);
-
-        bw.write(message);
-        bw.flush();
-    }
-
-    public String getMessage() throws IOException {
-        //Get the return message from the server
+    public String checkInput() throws java.io.IOException{
+        socket = serverSocket.accept();
         InputStream is = socket.getInputStream();
         InputStreamReader isr = new InputStreamReader(is);
         BufferedReader br = new BufferedReader(isr);
+
         String message = br.readLine();
         return message;
     }
 
+    public void sendOutput(String message) throws java.io.IOException{
+        OutputStream os = socket.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);
+       
+        bw.write(message);
+        System.out.println("Sending message: "+message);
+        bw.flush();
+    }
 
-    public static void main(String args[])
+    private static void optionsSelected(){
+        int temp = optionsSelected;
+
+        if(optionsSelected > 3){
+            confidentiality = true;
+            temp = optionsSelected - 4;
+        }else{
+            confidentiality = false;
+        }if(temp > 1){
+            integrity = true;
+            temp = temp - 2;
+        }else{
+            integrity = false; 
+        }
+        if(temp == 1){
+            authenticaiton = true;
+        }else{
+            authenticaiton = false; 
+        }
+    }
+
+    private static int getSecurity(){
+        Scanner scan = new Scanner(System.in);
+        boolean input = false; 
+
+        int option = 0;
+        int val = 0;
+
+        for(int i = 0; i < 3; i++){
+            input = false;
+            if(i == 0){
+                System.out.println("Would you like Confidentiality? (y/n)");
+                val = CONFIDENTIALITY;
+            }
+            else if(i == 1){
+                System.out.println("Would you like Integrity? (y/n)");
+                val = INTEGRITY;
+            }
+            else if(i == 2){
+                System.out.println("Would you like Authentication? (y/n)");
+                val = AUTHENTICATION;
+            } 
+            else{
+
+            }
+            while(!input){
+                char selection = scan.next().charAt(0);
+                if(selection == 'y'){
+                    option += val; 
+                    input = true;
+                }else if(selection == 'n'){
+                    input = true; 
+                }else{
+                    System.out.println("Please enter a valid option");
+                }
+
+            }
+        }
+        return option;
+    }
+
+    public static void main(String[] args)
     {
         try
         {
-            Client client = new Client();
-            client.sendMessage("test");
+            Server server = new Server();
+            optionsSelected = getSecurity();
 
-            String returnMessage = client.getMessage();
-            System.out.println("Message received from the server: " +returnMessage);
+            optionsSelected();
+            System.out.println("Confidentiality: "+confidentiality );
+            System.out.println("Integrity: "+integrity);
+            System.out.println("Authentication: "+authenticaiton);
 
+
+            while(true)
+            {
+                String message = server.checkInput();
+                System.out.println(message);
+
+
+                if(Integer.valueOf(message) == optionsSelected){
+                    System.out.println("Same security options have been selected"); 
+                }else{
+                    System.out.println("ERROR: Different security options have been selected"); 
+
+                }
+
+
+            }
         }
-        catch (Exception exception)
+        catch (Exception e)
         {
-            exception.printStackTrace();
+            e.printStackTrace();
         }
         finally
         {
@@ -57,10 +153,7 @@ public class Client
             {
                 socket.close();
             }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
+            catch(Exception e){}
         }
     }
 }
